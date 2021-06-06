e4c8c5360ec6b7cffbc5baf79b7bb8b880b3ca7f
Yashiry Azize <azizeyashiry@gmail.com>
2021-06-06 17:52:28 -0400

    completed lab and tests

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 const pokemon = require("./models/pokemon.json");
-console.log(pokemon[0]);
+
 
 app.get("/", (req, res) => {
-    res.send("Welcome 99 Pokemon")
-})
+  res.send("Welcome 99 Pokemon");
+});
 
 app.get("/:verb/:adjective/:noun", (req, res) => {
   const { verb, adjective, noun } = req.params;
@@ -15,29 +15,52 @@ app.get("/:verb/:adjective/:noun", (req, res) => {
 });
 
 app.get("/bugs", (req, res) => {
-   res.send(`<!DOCTYPE html>
+  res.send(`<!DOCTYPE html>
    <html>
    <h1>99 little bugs in the code</h1>
    <a href="/bugs/number_of_bugs">pull one down, patch it around</a>
-   </html>`)
+   </html>`);
 });
 
 app.get("/bugs/:number_of_bugs", (req, res) => {
-    const {number_of_bugs} = req.params
-    if(number_of_bugs >= 200) {
-        res.send(`<!DOCTYPE html>
+  const { number_of_bugs } = req.params;
+  if (number_of_bugs >= 200) {
+    res.send(`<!DOCTYPE html>
         <html>
         <a href="/bugs">Too many bugs!! Start over!</a>
-        </html>`)
-    } else {
-        res.send(`<!DOCTYPE html>
+        </html>`);
+  } else {
+    res.send(`<!DOCTYPE html>
         <html>
         <h1> ${number_of_bugs} little bugs in the code</h1>
-        <a href="/bugs/${Number(number_of_bugs) + 2}">Pull one down, patch it around</a>
-        </html>`)
-    }
+        <a href="/bugs/${
+          Number(number_of_bugs) + 2
+        }">Pull one down, patch it around</a>
+        </html>`);
+  }
+});
+
+app.get("/pokemon", (req, res) => {
+    res.send(pokemon);
+  
+});
+
+app.get("/pokemon/search", (req, res) => {
+    const { name } = req.query;
+    const matchingPokemon = pokemon.filter((p) => {
+        return p.name.toLowerCase() === name.toLowerCase();
+    })
+    res.send(matchingPokemon)
 })
 
+app.get("/pokemon/:indexOfArray", (req, res) => {
+    const { indexOfArray } = req.params;
+    if(!pokemon[indexOfArray]) {
+        res.status(404).send(`Sorry, no pokemon found at ${indexOfArray}`)
+    } else {
+        res.send(pokemon[indexOfArray])
+    }
+})
 
 
 module.exports = app;
