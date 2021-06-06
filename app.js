f50c3c109723229a3e6ade4351190972f291923c
Jeffrey <jeffreysebastian@pursuit.org>
2021-06-06 18:01:32 -0400

    created pokemon servers

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 const express = require("express");
 const app = express();
+const pokemon = require("./models/pokemon.json");
+// console.log(pokemon[0]);
 
 app.get("/", (req, res) => {
   res.send(`Welcome 99 Pokemon`);
 });
+
+app.get("/pokemon", (req, res) => {
+  res.json(pokemon[0])
+})
+
+app.get("/pokemon/search", (req, res)=> {
+  const {name} = req.query
+  const {search} = req.params
+  if(search){
+    res.json(pokemon[name])
+  }
+})
+app.get("/pokemon/:indexOfArray", (req, res)=>{
+  const{indexOfArray} = req.params
+  if (pokemon[indexOfArray]) {
+    res.json({
+      pokemon: pokemon[indexOfArray],
+    });
+  } else {
+    res.status(404).send(`Sorry, no pokemon found at ${pokemon[indexOfArray]}`);
+  }
+});
+
 
 app.get("/bugs", (req, res) => {
   res.send(`<h1>99 little bugs in the code</h1>
@@ -30,10 +55,12 @@ app.get("/bugs/:numberOfBugs", (req, res) => {
         <a href="/bugs/${
           Number(numberOfBugs) + Number(2)
         }">Pull one down, patch it around</a>`);
-  } else if (Number(numberOfBugs) > 200) {
+  } else {
     res.status(404).send(`<a href="/bugs">Too many bugs!! Start over!</a>`);
   }
 });
+
+
 
 module.exports = app;
 
