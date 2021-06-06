b5c4abcfc473944f71d94f2d10150e0f205f1a62
Obaid Rustemi <orustemi@gmail.com>
2021-06-06 12:44:04 -0400

    complete first portion of lab

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express()
+
+
+app.get("/:verb/:adjective/:noun", (req, res) => {
+    const { verb, adjective, noun} = req.params
+    res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app
