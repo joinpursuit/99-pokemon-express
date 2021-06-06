e03656fffb06725f65fe5ac0ef7e6e3346906ee8
Jonathan Bedon <bedonjonathan@gmail.com>
2021-06-06 11:56:04 -0400

    name generator project done

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/:verb/:adjective/:noun", (req, res) => {
+  const { verb, adjective, noun } = req.params;
+  res.send(
+    `Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`
+  );
+});
+
+module.exports = app;
