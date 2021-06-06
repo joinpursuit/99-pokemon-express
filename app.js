367bc3721da70f4aeccf2d934c0480f0d39dab92
Kobe <tahiriadam@gmail.com>
2021-06-06 18:16:18 -0400

    completed lab

--- app.js
+++ app.js
@@ -1,52 +1,62 @@
 const express = require("express");
 const app = express();
-const pokemon = require("./models/pokemon.json")
+const pokemon = require("./models/pokemon.json");
 
+//app.set('case sensitive routing', true);
 
 app.get("/:verb/:adjective/:noun", (req, res) => {
-    const { verb, adjective, noun } = req.params;
-    res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}`)
-})
+  const { verb, adjective, noun } = req.params;
+  res.send(
+    `Congratulations on starting a new project called ${verb}-${adjective}-${noun}`
+  );
+});
 
 app.get("/", (req, res) => {
-    res.send("Welcome 99 Pokemon")
-})
+  res.send("Welcome 99 Pokemon");
+});
 
 app.get("/bugs", (req, res) => {
-    res.send(`<h1>99 little bugs in the code<h1> <br> <a href="/bugs/101">pull one down, patch it around</a>`)
-})
-
-
+  res.send(
+    `<h1>99 little bugs in the code<h1> <br> <a href="/bugs/101">pull one down, patch it around</a>`
+  );
+});
 
 app.get("/bugs/:number_of_bugs", (req, res) => {
-    const { number_of_bugs } = req.params;
-    if(number_of_bugs < 200) {
-        res.send(`${number_of_bugs} little bugs in the code <br> <a href="/bugs/${Number(number_of_bugs) + 2}">Pull one down, patch it around</a>`)
-    } else {
-        res.send(`Too many bugs!! Start over! <br> <a href="/bugs">next</a>`)
-    }
-})
-
+  const { number_of_bugs } = req.params;
+  if (number_of_bugs < 200) {
+    res.send(
+      `${number_of_bugs} little bugs in the code <br> <a href="/bugs/${
+        Number(number_of_bugs) + 2
+      }">Pull one down, patch it around</a>`
+    );
+  } else {
+    res.send(`Too many bugs!! Start over! <br> <a href="/bugs">next</a>`);
+  }
+});
 
 app.get("/pokemon", (req, res) => {
-    res.json(pokemon)
-})
+  res.json(pokemon);
+});
+
+app.get("/pokemon/search", (req, res) => {
+  const { name } = req.query;
+  const pokemonFilter = pokemon.filter((char) => {
+       return name.toLowerCase() === char.name.toLowerCase();
+    });
+    if (pokemonFilter.length > 0) {
+      res.json(pokemonFilter);
+    } else {
+      res.send("[]");
+    }
+});
 
 app.get("/pokemon/:indexOfArray", (req, res) => {
-    const { indexOfArray } = req.params
-    if(pokemon[indexOfArray]) {
-        res.json(pokemon[indexOfArray])
-    } else {
-        res.send(`Sorry, no pokemon found at ${[indexOfArray]}`)
-    }
-})
+  const { indexOfArray } = req.params;
+  if (pokemon[indexOfArray]) {
+    res.json(pokemon[indexOfArray]);
+  } else {
+    res.send(`Sorry, no pokemon found at ${[indexOfArray]}`);
+  }
+});
 
-app.get("/pokemon/search/", (req, res) => {
-    const { name } = req.query
-    if(name) {
-    res.json(pokemon.name)
-    } else {
-        res.send("[]")
-    }
-})
 module.exports = app;
