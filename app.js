68ceb5ae1c281b564117fcdc871de70876170601
Daliz Cruz <dalizcruz@gmail.com>
2021-06-06 12:01:22 -0500

    poke-express

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require("express")
 const app = express();
 
-//New Project Name Generator
+//Poke-Express:
+const pokemon = require("./models/pokemon.json");
+
+app.get("/pokemon/:search", (req,res) => {
+    const { name } = req.query
+    const { search } = req.params
+    res.send(pokemon[name])
+})
+
+app.get("/pokemon", (req, res) => {
+    res.send(pokemon)
+})
+
+app.get("/pokemon/:indexOfArray", (req, res) => {
+    const { indexOfArray } = req.params
+    if(pokemon[indexOfArray]){
+        res.json({
+            pokemon: pokemon[indexOfArray]
+        })
+    }else {
+     res.send(`Sorry, no pokemon found at ${indexOfArray}` )       
+    }
+
+})
+
+
+
+//New Project Name Generator:
 app.get("/:verb/:adjective/:noun", (req, res) =>{
     const {verb, adjective, noun} = req.params
     res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}`)
 })
 
-//99 Little Bugs In the Code
+//99 Little Bugs In the Code:
 app.get("/bugs/101", (req, res) => {
     res.send("101 bugs in the code")
 })
@@ -28,6 +55,8 @@ app.get("/bugs", (req, res) =>{
     res.send(`<h1>99 little bugs in the code </h1>
               <a href="/bugs/101"> pull one down </a>  `)
 })
+
+
 
 
 
