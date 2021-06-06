455247cd372dcdc0f5ff6d148395a711a3d74610
Shoaib Dar <SDAR30@gmail.com>
2021-06-06 12:01:32 -0400

    first problem

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.send("welcome home")
+})
+
+app.get("/:verb/:adjective/:noun", (req,res)=>{
+    const {verb, adjective, noun} = req.params;
+    res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`)
+
+})
+
+module.exports = app;
