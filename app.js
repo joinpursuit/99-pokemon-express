bd475cd7bc23c163ce249c8a9055bc271bb1228e
Grace Bong <gracebong.park@gmail.com>
2021-06-08 14:26:25 -0400

    added bugs link

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const app = express();
 
+app.get('/bugs/:numberOfBugs', (require, response) => {
+    const { numberOfBugs } = require.params;
+    const number = Number(numberOfBugs);
+
+    if (number > 200) {
+        response.send(`
+
+            <a href=${`/bugs`}>Too many bugs!! Start over!</a>
+        `)
+    } else {
+        response.send(`
+            ${numberOfBugs} little bugs in the code
+            <a href=${`/bugs/${number+2}`}>pull one down, patch it around</a>
+        `)
+    }
+});    
+
+    
+
+app.get('/bugs', (require, response) => {
+    response.send(
+        `99 little bugs in the code
+        <a href=${`/bugs/101`}>pull one down, patch it around</a>`
+    )
+});
+
 app.get('/:verb/:adjective/:noun', (require, response) => {
     const { verb, adjective, noun } = require.params
-    response.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`)
+    response.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}`)
 });
 
 module.exports = app;
