529927d5ac7e3d3cd0d4f2ddf46ff0c06ef86dfe
Daniel Naranjo <MR.DANIEL.NARANJO@PROTONMAIL.COM>
2021-06-08 12:15:21 -0400

    App.js & Server.js implemented

---
+++ app.js
@@ -0,0 +1,19 @@
+// Dependencies
+const express = require("express");
+
+
+// Configuration
+const app = express();
+
+// Routes
+
+app.get("/jumping/joyous/jellybean", (req, res) => { 
+res.send("Congratulations on starting a new project called jumping-joyous-jellybean!")
+ })
+
+
+
+
+ // Export
+
+ module.exports = app;
