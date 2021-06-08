6a1b6331acd5682f5b1696719728542f252169c8
Alexander <alexanderl96@yahoo.com>
2021-06-08 13:09:48 -0400

    progress

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Pokemon");
+app.get("/jumping/joyous/jellybean", (req, res) => {
+    const verb = "jumping"
+    const adjective = "joyous"
+    const noun = "jellybean"
+    res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!
+    `);
   });
 
+  app.get("/bugs", (req, res) => {
+        res.send(`<h1>99 little bugs in the code</h1>`
+            )
+    })
 
 
-
-  module.exports = app
+module.exports = app
