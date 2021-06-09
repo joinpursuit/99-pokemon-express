a0e2a52072cb2b8a425ddfdc76e9d2f97d6bbf8f
Jenna Qian <jiayin1996@gmail.com>
2021-06-08 22:52:55 -0400

    Poke-Express Incomplete

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
-// const express = require("express");
-// const app = express();
-
-// app.get("/", (req,res) => {
-//     res.send("Welcome 99 Pokemon")
-// })
-
-// app.get("/bugs", (req,res) =>{
-//     res.send(`<h1>99 little bugs in the code</h1> <br></br> <a href="/bugs/101">pull one down,patch it around</a>`)
-// })
-
-// app.get("/jumping/joyous/jellybean", (req,res) => {
-//     res.send("Congratulations on starting a new project called jumping-joyous-jellybean!")
-// })
-
-// app.listen(8888)
-
 const express = require("express");
 const app = express();
+const pokemon = require("./models/pokemon.json")
+console.log(pokemon)
 
 app.get("/:verb/:adjective/:noun", (req, res) => {
   const { verb, adjective, noun } = req.params;
@@ -52,7 +37,28 @@ app.get("/bugs/:numberOfBugs", (req, res) => {
 });
 
 app.get("/pokemon", (req,res) => {
-    
+    res.send(pokemon)
 })
+app.get("/pokemon/search", (req,res) => {
+    const {name} = req.query
+    const caseIgnored = name.toLowerCase()
+    for(let i = 0; i < pokemon.length; i++){
+        if(caseIgnored === pokemon[i].caseIgnored) {
+            res.send([pokemon[i]]);
+        } else {
+            res.send([])
+        }
+    }
+})
+
+app.get("/pokemon/:indexOfArray", (req,res) => {
+    const {indexOfArray} = req.params
+    if(pokemon[indexOfArray]) {
+        res.send(pokemon[indexOfArray])
+    } else {
+        res.send(`Sorry, no pokemon found at ${indexOfArray}`)
+    }
+})
+
 
 module.exports = app;
