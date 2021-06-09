62132a8d0dd03664d942c78d0b2b37a111f75e65
cassidybeni <cassidybeni@pursuit.org>
2021-06-08 21:55:17 -0400

    Pass 8 tests

--- app.js
+++ app.js
@@ -1,9 +1,52 @@
 const express = require("express");
 const app = express();
+const pokemon = require("./models/pokemon.json");
 
-app.get("/:adjective/:verb/:noun", (req, res) => {
-    const { adjective, verb, noun} = req.params
-  res.send(`Congratulations on starting a new project called ${adjective}-${verb}-${noun}!`);
+app.get("/pokemon", (req, res) => {
+  res.send(pokemon);
 });
 
-module.exports = app
+app.get("/pokemon/:indexOfArray", (req, res) => {
+  const { indexOfArray } = req.params;
+  if(pokemon[indexOfArray]){
+    res.send(pokemon[indexOfArray]);
+  } else {
+    res.send(`Sorry, no pokemon found at ${indexOfArray}`)
+  }
+});
+
+app.get("/pokemon/search", (req, res) => {
+  const { name } = req.query
+  res.send(name)
+})
+
+app.get("/bugs/:numberOfBugs", (req, res) => {
+  const { numberOfBugs } = req.params;
+
+  if (numberOfBugs < 200) {
+    res.send(`${numberOfBugs} little bugs in the code <a href=${`/bugs/${Number(numberOfBugs) + 2}`}>Pull one down, patch it around</a>`);
+  } else {
+    res.send(
+     `<a href=${"/bugs"}>Too many bugs!! Start over!</a>`
+    );
+  }
+});
+
+app.get("/bugs", (req, res) => {
+  res.send(
+    `99 little bugs in the code <a href=${"/bugs/101"}>pull one down, patch it around</a>`
+  );
+});
+
+app.get("/:verb/:adjective/:noun", (req, res) => {
+  const { adjective, verb, noun } = req.params;
+  res.send(
+    `Congratulations on starting a new project called ${verb}-${adjective}-${noun}`
+  );
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome 99 Pokemon");
+});
+
+module.exports = app;
