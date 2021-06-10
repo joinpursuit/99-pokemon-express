cdc86d36bc4dc623a80b5ff364e259bea04e01fd
Geormary Sanchez <geormarysanchez.gs@gmail.com>
2021-06-10 16:49:22 -0400

    passing all tests

--- app.js
+++ app.js
@@ -1,14 +1,58 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get("/:verb/:adj/:noun",(req, res)=>{
-    const {verb, adj, noun} = req.params
+const pokemon = require("./models/pokemon.json");
 
-    console.log(req.params)
+app.get("/:verb/:adj/:noun", (req, res) => {
+  const { verb, adj, noun } = req.params;
+  res.send(
+    `Congratulations on starting a new project called ${verb}-${adj}-${noun}`
+  );
+});
 
-    res.send(`Congratulations on starting a new project called ${verb}-${adj}-${noun}!`)
+app.get("/", (req, res) => {
+  res.send(`Welcome 99 Pokemon`);
+});
 
-})
+app.get("/bugs", (req, res) => {
+  // const link = <a href="">pull one down, patch it around</a>
+  res.send(
+    `<h1> 99 little bugs in the code</h1> <a href="/bugs/101">pull one down, patch it around</a>`
+  );
+});
 
+app.get("/bugs/:numberOfBugs", (req, res) => {
+  const { numberOfBugs } = req.params;
+  if (numberOfBugs >= 200) {
+    res.send(`<a href="/bugs">Too many bugs!! Start over!</a>`);
+  } else {
+    res.send(
+      `<h1>${numberOfBugs} little bugs in the code </h1><a href="/bugs/${
+        Number(numberOfBugs) + 2
+      }">Pull one down, patch it around</a>`
+    );
+  }
+});
+
+app.get("/pokemon", (req, res) => {
+  res.json(pokemon);
+});
+
+app.get("/pokemon/search", (req, res) => {
+  const { name } = req.query;
+  const result = pokemon.filter(
+    (poke) => poke.name.toLowerCase() === name.toLowerCase()
+  );
+  res.send(result);
+});
+
+app.get("/pokemon/:index", (req, res) => {
+  const { index } = req.params;
+  if (pokemon[index]) {
+    res.status(200).json(pokemon[index]);
+  } else {
+    res.send(`Sorry, no pokemon found at ${index}`);
+  }
+});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -7,3 +7,4 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
