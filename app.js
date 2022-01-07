db4ba3a54a6881339a520933ba136de2fd5aa3d1
Christina Loiacono <loiaconochristina@gmail.com>
2022-01-07 12:33:15 -0500

    New Project Name Generator complete

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const app = express();
 
 // Routes
 app.get("/", (req, res)=>{
-    res.send("<h1>Hello</h1>");
+    res.send("Welcome 99 Pokemon");
+});
+
+app.get("/:verb/:adjective/:noun", (req, res)=>{
+    let { verb, adjective, noun } = req.params;
+    res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`);
 });
 
 // Export
