3d31d0f4255031d186a1ee818bd72a46a0c85a4b
stephanie lucano <stephaniemarielucano@gmail.com>
2022-01-07 14:52:26 -0500

    Send a response back to the client with the new project name

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ const express = require("express")
 const app = express()
 
 // New Project Name Generator
-app.get("/:verb/:adjective/:noun", () => {})
+app.get("/:verb/:adjective/:noun", (request, response) => {
+    response.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`)
+})
