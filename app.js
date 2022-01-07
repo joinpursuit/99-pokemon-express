f36f89bb2bfa88f6a10bacf5bc990584f983e117
Jalamang <Jalamangjanka@gmail.com>
2022-01-06 20:00:43 -0500

    added /bugs and /bugs/:number routes and  if else condition with a hyperlink that pulls down a bug when the bugs left  is less than 200 otherswise an hyperlink to start over (routes to /bugs)

--- app.js
+++ app.js
@@ -7,4 +7,19 @@ app.get("/", (request, response) => {
   });
 
 
+  //Route that takes 3 parameters in the URL
+app.get("/:verb/:adjective/:noun", (request, response) => {
+    const {verb, adjective, noun} = request.params
+    console.log(verb)
+    console.log("GET request received to route:  /verb/:adjective/:noun");
+     response.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}`);
+  });
+
+//Route that takes 3 parameters in the URL
+app.get("/bugs", (request, response) => {
+    console.log("GET request received to route:  /bugs");
+     response.send("99 little bugs in the code");
+  });
+
+
 module.exports = app;
