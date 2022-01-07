284d34b6bfed05e7290e943ce8d4afcc158038a3
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2022-01-07 12:48:46 -0500

    passed 2 tests

--- app.js
+++ app.js
@@ -9,14 +9,24 @@ app.get('/bugs', (request, response) => {
     response.send("<h1> 99 little bugs in the code </h1>");
 });
 
-// app.get('/bugs/:numberOfBugs', (request, response) => {
-//     const { numberOfBugs } = request.params;
-//     if ()
-// });
+app.get('/bugs/:numberOfBugs', (request, response) => {
+    const numberOfBugs = Number(request.params.numberOfBugs);
+    if (numberOfBugs >= 200) {
+        response.send("Too many bugs!! Start over!");
+    } else {
+        response.send(`
+        ${numberOfBugs} little bugs in the code
+        <a 
+            href='http://localhost:8888/bugs/${numberOfBugs + 2}'>
+            next
+        </a>
+        `);
+    }
+});
 
 app.get('/:verb/:adjective/:noun', (request, response) => {
     const { verb, adjective, noun } = request.params;
-    if (verb === 'jumping' && adjective === 'joyous' && noun === 'jellybean') {
+    if (verb && adjective && noun) {
         response.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`);
     };
 });
