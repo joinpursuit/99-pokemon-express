bed5e33fde5ba58a247d2379c5458089e9df93b8
stephanie lucano <stephaniemarielucano@gmail.com>
2022-01-07 15:02:54 -0500

    Deconstruct the request object to get the value of verb, adjective, and noun

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 
 // New Project Name Generator
 app.get("/:verb/:adjective/:noun", (request, response) => {
+    const { verb, adjective, noun } = request.params
     response.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`)
 })
 
