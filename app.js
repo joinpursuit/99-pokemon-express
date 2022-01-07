6e0c6cf8ffb5a05fd06d40aa49d6af60cd1bda7a
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-01-07 14:26:02 -0500

    some bug section completed, added DS to git ignore

--- app.js
+++ app.js
@@ -4,8 +4,23 @@ const app = express();
 
 //routes
 app.get("/", (req, res) => {
-    res.send("<h1> Server is seding response </h1>");
+    res.send("Welcome 99 Pokemon");
 });
+
+//~new project name generator
+app.get("/:verb/:adjective/:noun", (req, res) => {
+    const { verb, adjective, noun } = req.params;
+    res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`);
+});
+
+//~99 little bugs 
+app.get("/bugs", (req, res) => {
+    res.send("<h1>99 little bugs in the code</h1>");
+});
+
+app.get("/bugs/:numberOfBugs", (req, res) => {
+    
+})
 
 //export 
 module.exports = app;
