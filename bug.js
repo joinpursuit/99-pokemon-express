3eebf770dda15d97697a293e026e36a53ee534cd
Lakisha J <lakishajohnson@pursuit.org>
2022-01-07 10:59:34 -0500

    Small change to array

--- bug.js
+++ bug.js
@@ -1,9 +1,9 @@
 const bug = [
 "99 little bugs in the code",
 "99 little bugs",
-"Pull one down",
-"Patch it around",
+"Pull one down, patch it around",
 "101 bugs in the code",
+"Too many bugs!! Start over!",
 ]
 
 module.exports = bug
