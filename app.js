0382f802db069115e68928544d7bcdaa04998187
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-01-07 09:30:15 -0500

    added error/next link

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 const express = require("express");
 const app = express();
-// const rock = require("./models/rock");
+const pokemon = require("./models/pokemon.json");
 
-// Routes
 app.get("/", (request, response) => {
-      console.log("Get request received to route: /");
-      response.send("Welcome 99 Pokemon");
-    });
-    
-    app.get("/:verb/:adjective/:noun", (request, response) => {
-        const { verb, adjective, noun } = request.params
-        response.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`)
-    })
-app.get("/bugs", (request, response) => {
-  response.send("99 little bugs in the code");
+  console.log("GET request received to route: /");
+  response.send("Welcome 99 Pokemon");
 });
 
-app.get("/bugs/:numberOfBugs", (request, response) => {
+app.get("/:verb/:adjective/:noun", (request, response) => {
+  const { verb, adjective, noun } = request.params;
   response.send(
-    "Rock number " + request.params.index + " is: " + rock[request.params.index]
+    `Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`
   );
 });
 
+app.get("/bugs", (request, response) => {
+  response.send(`<h1>99 little bugs in the code</h1>
+            <a href="101">pull one down, patch it around</a>`);
+});
+
+app.get("/bugs/:numberOfBugs", (request, response) => {
+  const { numberOfBugs } = request.params;
+  const numOfBugs = Number(numberOfBugs);
+  response.send(`<h1>${numOfBugs} little bugs in the code</h1>
+  <a href="${numOfBugs < 200 ? numOfBugs + 2 : "./"}">${
+    numOfBugs < 200
+      ? "Pull one down, patch it around"
+      : "Too many bugs!! Start over!"
+  }</a>`);
+});
 
 module.exports = app;
