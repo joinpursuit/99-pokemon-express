7186629b01682176cb12d597ee2d464856e0d6f2
Nima sherpa <Toplouie78@gmail.com>
2022-01-07 12:22:54 -0500

    the server is working properly

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 // requiring express
 const express = require("express");
+const res = require("express/lib/response");
 // calling express
 const app = express();
 
 // inorder to use the dotenv...
 require("dotenv").config();
+
+app.get("/", (req, res) => {
+	res.send("The data has been send");
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+	console.log(` Listening on port ${PORT}  `);
+});
