2adf32abfffa4845f2867713a9103dec2b373694
Melissa Lopez <1zuzsuit492@Melissas-MacBook-Pro.local>
2022-01-07 14:10:11 -0500

    .

--- app.js
+++ app.js
@@ -10,15 +10,14 @@ response.send("Welcome 99 Pokemon");
 
 //99 little bugs in the code
 app.get("/bugs", (request, response) => {
-response.send(`99 little bugs in the code <br> <a href="/bugs/101">pull one down, patch it around</a>`);
+response.send(`<h1>99 little bugs in the code <h1> <br> <a href="/bugs/101">pull one down, patch it around</a>`);
 });
 
 app.get("/bugs/:numberOfBugs", (request, response) => {
 const { numberOfBugs } = request.params;
     if (numberOfBugs < 200) { //if less than 200 bugs, pull one down
     response.send(`${numberOfBugs} little bugs in the code <br> 
-    <a href="/bugs/${Number(numberOfBugs) + 2
-    }">Pull one down, patch it around</a>`
+    <a href="/bugs/${Number(numberOfBugs) + 2} ">Pull one down, patch it around</a>`
     );} 
     else {
     response.send(`Too many bugs!! Start over! <br> <a href="/bugs">Start Over</a>`);
@@ -45,15 +44,21 @@ response.json(pokemon[indexOfArray]); //return pokemon at that position
 else {
 response.send(`Sorry, no pokemon found at ${[indexOfArray]}`);
     } //no pokemon at that position
-  });
+});
 
-
-
-
-
-
-
-
+// make a route /pokemon/search - where a user can add a query parameter
+app.get("/pokemon/search", (request, response) => {
+    const { poke } = request.query;
+    const pokemonArr = pokemon.filter((pikachu) => {
+    return poke.toLowerCase() === pikachu.name.toLowerCase(); //not case sensitive
+});
+    if (pokemonArr.length > 0) {
+    response.json(pokemonArr);
+    } 
+    else {
+    response.send("[]"); //show empty arr if pokemon isnt found
+    }
+});
 
 
 module.exports = app;
