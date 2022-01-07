82a4569e4c858e3223e58b1f439dba7b9b3f1be3
Rohan-Sterling <rohansterling@pursuit.org>
2022-01-06 22:47:06 -0500

    import express, add route for verb, adjective and noun, add route status console log, addresponse.send message

---
+++ app.js
@@ -0,0 +1,9 @@
+import express from "express"
+
+const app = express()
+
+app.get('/:verb/:adjective/:noun', (req, res) => {
+    console.log("Get /:verb/:adjective/:noun")
+    const { verb, adjective, noun} = req.params
+    res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`)
+})
