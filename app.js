733926d069e5bbad5ebcef3d6eb168fb8ef05350
Lakisha J <lakishajohnson@pursuit.org>
2022-01-07 14:27:11 -0500

    Tests passing

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
+// DEPENDENCIES
 const express = require("express");
-const peach = express();
 const bug = require("./bug");
 const pokemon = require("./models/pokemon.json")
 // console.log(pokemon[0])
+// CONFIGURATION
+const app = express();
 
+// ROUTES
 http://localhost:8888
-peach.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Welcome 99 Pokemon");
 });
 // http://localhost:8888/jumping/joyous/jellybean
-peach.get("/:verb/:adjective/:noun", (req, res) => {
+app.get("/:verb/:adjective/:noun", (req, res) => {
   res.send(
     `Congratulations on starting a new project called ${req.params.verb}-${req.params.adjective}-${req.params.noun}!`
   );
 });
 // http://localhost:8888/bugs
-peach.get("/bugs", (req, res) => {
+app.get("/bugs", (req, res) => {
   console.log("Get request received to route: /bugs");
   // res.send(bug[0])
   res.send(`${bug[0]} <a href="/bugs/101">${bug[2]}`);
 });
 // http://localhost:8888/bugs/1000
-peach.get("/bugs/:numberOfBugs", (request, response) => {
+app.get("/bugs/:numberOfBugs", (request, response) => {
   const { numberOfBugs } = request.params;
   const numOfBugs = Number(numberOfBugs);
 
   response.send(`<p>${numOfBugs} little bugs in the code</p>
-    <a href="${numOfBugs < 200 ? numOfBugs + 2 : "/"}">
-    ${
+    <a href="${numOfBugs < 200 ? numOfBugs + 2 : "/"}">${
     numOfBugs < 200
       ? `${bug[2]}`
       : `${bug[4]}`
@@ -35,11 +37,11 @@ peach.get("/bugs/:numberOfBugs", (request, response) => {
   </a>`);
 });
 //  http://localhost:8888/pokemon
-peach.get("/pokemon", (req, res) => {
+app.get("/pokemon", (req, res) => {
     res.send(pokemon);
   });
 //   http://localhost:8888/pokemon/search?name=oddish
-  peach.get("/pokemon/search", (request, response) => {
+  app.get("/pokemon/search", (request, response) => {
     // const { name } = request.query;
     const poki = pokemon.find(
       (pokemon) => pokemon.name.toLowerCase() === request.query.name.toLowerCase()
@@ -47,10 +49,13 @@ peach.get("/pokemon", (req, res) => {
     response.send(poki ? [poki] : []);
   });
 // http://localhost:8888/pokemon/5
-  peach.get("/pokemon/:index", (request, response) => {
+  app.get("/pokemon/:index", (request, response) => {
     const { index } = request.params;
     response.send(
       pokemon[Number(index)] || "Sorry, no pokemon found at " + index
     );
   });
-module.exports = peach;
+
+
+//   EXPORT
+module.exports = app;
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+// DEPENDENCIES
 const dotenv = require("dotenv")
 const mango = require("./app");
 
+// CONFIGURATION
 dotenv.config()
-
 const PORT = process.env.PORT
+
+// LISTEN
 mango.listen(PORT, () => {
     console.log(`Listening on Port: ${PORT}`)
 })
