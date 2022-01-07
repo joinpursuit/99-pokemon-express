e2ba1958f50814b0117000de50866e6a49da9429
salrey <tekdehn@gmail.com>
2022-01-07 12:00:05 -0500

    Setup Poke-Express

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //DEPENDENCIES 
+const e = require("express");
 const express = require("express");
-
+const pokemon = require("./models/pokemon.json");
+// console.log(pokemon[0]);
 
 //CONFIGURATION
 const app = express();
@@ -11,11 +13,16 @@ const app = express();
 app.get("/:verb/:adjective/:noun", (request, response) => {
     console.log("GET request received to route: /:verb/:adjective/:noun")
     const { verb, adjective, noun } = request.params
-    response.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`)
+    response.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}`)
 })
 
 //99 Little Bugs In the Code
 const bugs = 99
+app.get("/", (request, response) => {
+    console.log("GET request received to route: /")
+    response.send(`Welcome ${bugs} Pokemon`)
+})
+
 app.get("/bugs", (request, response) => {
     console.log("GET request received to route: /bugs")
     response.send(`
@@ -31,14 +38,38 @@ app.get("/bugs/:numberOfBugs", (request, response) => {
     if (numberOfBugs < 200) {
         response.send(`
         <p>${numberOfBugs} little bugs in the code</p>
-        <a href="/bugs/${sum}">pull one down, patch it around</a>
+        <a href="/bugs/${sum}">Pull one down, patch it around</a>
         `)
     } else {
         response.send(`
-        <p>${numberOfBugs} little bugs in the code</p>
-        <a href="/bugs">Start Over</a>
+        <a href="/bugs">Too many bugs!! Start over!</a>
         `)
     }
 })
+
+//Poke-Express
+app.get("/pokemon", (request, response) => {
+    console.log("GET request received to route: /pokemon")
+    response.send(pokemon)
+})
+
+app.get("/pokemon/search", (request, response) => {
+    console.log("GET request received to route: /pokemon/search")
+    console.log(request.query)
+    const { name } = request.query
+    response.send(pokemon.filter((pokemon) => pokemon.name.toLowerCase() === name.toLowerCase()))
+})
+
+app.get("/pokemon/:indexOfArray", (request, response) => {
+    console.log("GET request received to route: /pokemon/:indexOfArray")
+    const { indexOfArray } = request.params
+    if (pokemon[indexOfArray]) {
+        response.send(pokemon[indexOfArray])
+    } else {
+        response.send(`Sorry, no pokemon found at ${[indexOfArray]}`)
+    }
+})
+
+
 
 module.exports = app;
