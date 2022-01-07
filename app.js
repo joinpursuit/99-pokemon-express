db5bfd5385c63d560bac87a5a7e150c16d6060ce
Jalamang <Jalamangjanka@gmail.com>
2022-01-06 20:03:10 -0500

    99 Little Bugs In the Code

--- app.js
+++ app.js
@@ -2,24 +2,37 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-    console.log("GET request received to route:  /");
-    response.send("Welcome 99 Pokemon");
-  });
-
-
-  //Route that takes 3 parameters in the URL
-app.get("/:verb/:adjective/:noun", (request, response) => {
-    const {verb, adjective, noun} = request.params
-    console.log(verb)
-    console.log("GET request received to route:  /verb/:adjective/:noun");
-     response.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}`);
-  });
+  console.log("GET request received to route:  /");
+  response.send("Welcome 99 Pokemon");
+});
 
 //Route that takes 3 parameters in the URL
-app.get("/bugs", (request, response) => {
-    console.log("GET request received to route:  /bugs");
-     response.send("99 little bugs in the code");
-  });
+app.get("/:verb/:adjective/:noun", (request, response) => {
+  const { verb, adjective, noun } = request.params;
+  console.log(verb);
+  console.log("GET request received to route:  /verb/:adjective/:noun");
+  response.send(
+    `Congratulations on starting a new project called ${verb}-${adjective}-${noun}`
+  );
+});
 
+//99 Little Bugs In the Code
+app.get("/bugs", (request, response) => {
+  console.log("GET request received to route:  /bugs");
+  response.send("99 little bugs in the code");
+});
+
+app.get("/bugs/:number", (request, response) => {
+  const { number } = request.params;
+  if (number < 200) {
+    response.send(
+      `${number} little bugs in the code <a href=/bugs/${
+        Number(number) + 2
+      }> Pull one down, patch it around</a>`
+    );
+  } else {
+    response.send("<a href=/bugs>Too many bugs!! Start over!</a>");
+  }
+});
 
 module.exports = app;
