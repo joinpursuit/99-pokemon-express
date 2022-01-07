0bb2482a67df353caa35700c43abbe64153f6eb7
Melissa Lopez <1zuzsuit492@Melissas-MacBook-Pro.local>
2022-01-07 12:18:51 -0500

    passing poke index

--- app.js
+++ app.js
@@ -3,30 +3,51 @@ const express = require("express");
 const app = express();
 const pokemon = require("./models/pokemon.json");
 
+//intro page
 app.get("/", (request, response) => { 
 response.send("Welcome 99 Pokemon");
 })
 
+//99 little bugs in the code
 app.get("/bugs", (request, response) => {
 response.send(`99 little bugs in the code <br> <a href="/bugs/101">pull one down, patch it around</a>`);
 });
 
-app.get("/bugs/:number_of_bugs", (request, response) => {
-    const { number_of_bugs } = request.params;
-    if (number_of_bugs < 200) {
-    response.send(`${number_of_bugs} little bugs in the code <br> <a href="/bugs/${Number(number_of_bugs) + 2
+app.get("/bugs/:numberOfBugs", (request, response) => {
+const { numberOfBugs } = request.params;
+    if (numberOfBugs < 200) { //if less than 200 bugs, pull one down
+    response.send(`${numberOfBugs} little bugs in the code <br> 
+    <a href="/bugs/${Number(numberOfBugs) + 2
     }">Pull one down, patch it around</a>`
     );} 
     else {
-      response.send(`Too many bugs!! Start over! <br> <a href="/bugs">Start Over</a>`);
+    response.send(`Too many bugs!! Start over! <br> <a href="/bugs">Start Over</a>`);
     }
-  });
-  
+});
 
+//Congratulations on starting a new project called jumping-joyous-jellybean!
 app.get("/:verb/:adjective/:noun", (request, response) => {
 const { verb, adjective, noun } = request.params;
 response.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`)
 })
+
+// make a route `/pokemon` that will show a list of all the pokemon
+app.get("/pokemon", (request, response) => {
+response.json(pokemon)    
+})
+
+// make a route `/pokemon/:indexOfArray` that returns 1 pokemon at that array position
+app.get("/pokemon/:indexOfArray", (request, response) => {
+const { indexOfArray } = request.params; //user input?
+if (pokemon[indexOfArray]) {
+response.json(pokemon[indexOfArray]); //return pokemon at that position
+} 
+else {
+response.send(`Sorry, no pokemon found at ${[indexOfArray]}`);
+    } //no pokemon at that position
+  });
+
+
 
 
 
