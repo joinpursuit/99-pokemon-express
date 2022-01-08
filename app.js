178c16b974e2f8ddd19c51b45d71285d94038f1e
Nima sherpa <Toplouie78@gmail.com>
2022-01-07 20:45:45 -0500

    passing 5 test

---
+++ app.js
@@ -0,0 +1,67 @@
+const e = require("express");
+const express = require("express");
+const res = require("express/lib/response");
+// calling express
+const app = express();
+const pokemon = require("./models/pokemon.json");
+
+app.get("/", (req, res) => {
+	res.send("Welcome 99 Pokemon");
+});
+
+// The Name generator
+app.get("/:verb/:adjective/:noun", (req, res) => {
+	let { verb, adjective, noun } = req.params;
+	res.send(
+		`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`
+	);
+});
+
+// The bug fix - first step --
+app.get("/bugs", (req, res) => {
+	res.send(
+		`99 little bugs in the code <a href="/bugs/101">pull one down, patch it around</a>`
+	);
+	// res.send(`<a href="/bugs/101">pull one down</a>`);
+});
+
+// tryna get the number to show up.
+app.get("/bugs/:numberOfBugs"),
+	(req, res) => {
+		let { numberOfBugs } = req.params;
+		if (numberOfBugs >= 200) {
+			res.send(` <a href="/">Too many bugs!! Start over</a>`);
+			return;
+		}
+		res.send(numberOfBugs + 2);
+
+		// res.send(String(numberOfBugs) + "little bugs in the code");
+	};
+
+// The pokemon app
+
+app.get("/pokemon", (req, res) => {
+	res.send(pokemon);
+});
+
+// sending a empty array when the pokemon isnt found ..
+app.get("/pokemon/:search", (req, res) => {
+	let { search } = req.params;
+	pokemon.find((e) => (e === e.name ? res.json(e) : res.send([])));
+});
+
+// getting the pokemon depending on its index
+app.get("/pokemon/:index", (req, res) => {
+	let { index } = req.params;
+	if (pokemon[index]) {
+		res.send(pokemon[req.params.index]);
+	} else {
+		res.send(`Sorry, no pokemon found at ${index}`);
+	}
+});
+
+console.log(pokemon[0]);
+
+module.exports = app;
+
+// res.send("Your solution is " + (Number(num1) + Number(num2)));
--- server.js
+++ server.js
@@ -1,38 +1,8 @@
-// requiring express
-const express = require("express");
-const res = require("express/lib/response");
-// calling express
-const app = express();
-
+const app = require("./app.js");
 // inorder to use the dotenv...
 require("dotenv").config();
-
-app.get("/", (req, res) => {
-	res.send("The data has been send");
-});
-
-// The Name generator
-app.get("/:verb/:adjective/:noun", (req, res) => {
-	let { verb, adjective, noun } = req.params;
-	res.send(
-		`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`
-	);
-});
-
-// The bug fix
-app.get("/bugs", (req, res) => {
-	res.send(`99 little bugs in the code <a href="/bugs/101">pull one down</a>`);
-	// res.send(`<a href="/bugs/101">pull one down</a>`);
-});
-
+// for listening in the port ...
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
 	console.log(` Listening on port ${PORT}  `);
 });
-
-// Congratulations on starting a new project called jumping-joyous-jellybean!
-
-// On the home page (get "/bugs"), users should see:
-//
-// a link that says "pull one down, patch it around"
-// this should link to /bugs/101, where the number represents the number of bugs remaining to fix
