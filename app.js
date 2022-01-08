52ccb71e997a861162e744d24d7b4d781767c8c6
Christina Loiacono <loiaconochristina@gmail.com>
2022-01-08 15:22:12 -0500

    made search that works only for bulbasaur

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Dependencies
+const e = require('express');
 const express = require('express');
 
 // Configuration
@@ -44,27 +45,45 @@ app.get("/pokemon", (req, res)=>{
 })
 
 app.get(`/pokemon/search`, (req, res)=>{
-    console.log("this is req.params", req.params);
-    console.log("this is req.query", req.query);
+    const { name } = req.query;
+    console.log("this is req.params: ", req.params);
+    console.log("this is req.query: ", req.query);
+    console.log("this is req.query.name: ", req.query.name);
+    console.log("this is a pokemon name: ", pokemon[0].name.toLowerCase());
+    // console.log("this is a pokemon obj: ", pokemon[0]);
 
-    console.log("this is req.query.name", req.query.name);
-    console.log("this is a pokemon name", pokemon[0].name.toLowerCase());
-    // console.log("this is a pokemon obj", pokemon[0]);
+    
 
-    let count = -1;
-    for (let pokeIndex of pokemon) {
-        count += 1;
-        // console.log(count, pokeIndex.name.toLowerCase());
-    }
-    console.log("the count is now:", count)
+    // ***************************  START: Only works for bulbasaur vvv
+    // let showThisArr = [];
+    // for (let poke of pokemon) {
+    //     // console.log("poke: ", poke); // loops through every pokemon obj
+    //     // console.log("poke: ", poke.name); // loops through every pokemon obj.name
+    //     if (poke.name.toLowerCase() === req.query.name) {
+    //         showThisArr.push(poke)
+    //     } else {
+    //         showThisArr = [];
+    //     }
+    //     res.send(showThisArr);
+    // }
+    // *************************** END: Only works for bulbasaur ^^^
 
-    if (req.query.name === pokemon[0].name.toLowerCase()) {
-        console.log("confused");
-        res.json([pokemon[0]]);
-    } else {
-        console.log("when pokemon isnt found - name is missingno");
-        res.json([]);
-    }    
+    pokemon
+
+    // let count = -1;
+    // for (let pokeIndex of pokemon) {
+    //     count += 1;
+    //     // console.log(count, pokeIndex.name.toLowerCase());
+    // }
+    // console.log("the count is now:", count)
+
+    // if (req.query.name === pokemon[0].name.toLowerCase()) {
+    //     console.log("confused");
+    //     res.json([pokemon[0]]);
+    // } else {
+    //     console.log("when pokemon isnt found - name is missingno");
+    //     res.json([]);
+    // }    
 })
 
 app.get("/pokemon/:indexOfArray", (req, res)=>{
