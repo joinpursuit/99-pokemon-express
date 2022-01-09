03b2cca7b70e467d4993eb427b5d53641f7450ff
stephanie lucano <stephaniemarielucano@gmail.com>
2022-01-09 02:45:03 -0500

    Removed the exclamation mark from the response message to pass the test, thank you Samantha for pointing that out!

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 // New Project Name Generator
 app.get("/:verb/:adjective/:noun", (request, response) => {
     const { verb, adjective, noun } = request.params
-    response.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`)
+    response.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}`)
 })
 
 // 99 Little Bugs In The Code
