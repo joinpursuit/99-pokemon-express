637df42a441ae257c63038dedefdf87fd84f6a92
“Lili <lilyhuang114classb@gmail.com>
2022-01-09 14:20:57 -0500

    Fixed the array issue in res.send.

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const res = require("express/lib/response");
+const pokemon = require("./models/pokemon.json");
+
+// console.log(pokemon[0])
 
 const app = express();
 
@@ -13,17 +17,46 @@ app.get("/:verb/:adjective/:noun", (req, res)=> {
 
 app.get("/bugs", (req, res)=> {
     res.send(`<h1>99 little bugs in the code.</h1>
-    <a href="http://localhost:8888/bugs/"101>pull one down, patch it around</a>")`
+    <a href="http://localhost:8888/bugs/101">pull one down, patch it around</a>`
     );
 })
 
 app.get("/bugs/:numberOfBugs",(req, res)=> {
     const { numberOfBugs } = req.params;
-    
-        res.send(`<h1>${numberOfBugs} little bugs in the code</h1>
-        <a href="http://localhost:8888/bugs/"${numberOfBugs}>pull one down, patch it around</a>")`
+    let num = Number(numberOfBugs) + 2
+    if( numberOfBugs >= 200 ) {
+        res.send(`<a href="http://localhost:8888/bugs">Too many bugs!! Start over!</a>`)
+    } else {
+        res.send(`${numberOfBugs} little bugs in the code
+        <a href="http://localhost:8888/bugs/${num}">Pull one down, patch it around</a>`
         );
-    
+    }
 })
+
+app.get("/pokemon",(req, res) => {
+    res.send(pokemon);
+})
+
+app.get("/pokemon/search", (req, res) => {
+    const { name } = req.query;
+    for (let pokemonObj of pokemon) {
+        if(name.toLowerCase() === pokemonObj.name.toLowerCase()) {
+            res.send([pokemonObj]);
+        } else {
+            res.send([])
+        }
+    }
+})
+
+app.get("/pokemon/:indexOfArray",(req, res) => {
+    const { indexOfArray } = req.params;
+    if(pokemon[indexOfArray]) {
+        res.send(pokemon[indexOfArray]);
+    } else {
+        res.send(`Sorry, no pokemon found at ${indexOfArray}`)
+    }
+})
+
+
 
 module.exports = app;
