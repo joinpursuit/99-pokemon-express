8d9dfeee63a374bb5aa188fe4a1ed2e2115e9ffc
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2022-01-10 06:24:48 -0500

    this was confusing

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.get("/bugs", (request, response) => {
 
 app.get("/bugs/:numOfBugs", (request, response) => {
     let { numOfBugs } = request.params;
+    console.log(`GET request received for /bugs/${numOfBugs} route`);
     if (numOfBugs < 200) {
         let plusTwo = Number(numOfBugs) + 2;
         response.send(`${numOfBugs} little bugs in the code <a href="http://localhost:8888/bugs/${plusTwo}">Pull one down, patch it around</a>`);
@@ -23,12 +24,44 @@ app.get("/bugs/:numOfBugs", (request, response) => {
     }
 });
 
-app.get("/pokemon", (request, response)=>{
+app.get("/pokemon", (request, response) => {
+    console.log("GET request received for /pokemon route");
     response.json(pokemon);
+});
+
+app.get("/pokemon/search", (request, response) => {
+    const { name } = request.query;
+    const pokemonName = pokemon.find((poke) => {
+        return poke.name.toLowerCase() === name.toLowerCase()
+    });
+    if (pokemonName) {
+        response.json([pokemonName]);
+    } else {
+        response.json([]);
+    }
+});
+
+// app.get("/pokemon/search", (request, response) => {
+//     const { name } = request.query;
+//     const pokemonName = pokemon.find(
+//     (poke) => poke.name.toLowerCase() === name.toLowerCase()
+//     );
+//     response.send(pokemonName ? [pokemonName] : []);
+// });
+
+app.get("/pokemon/:index", (request, response) => {
+    let { index } = request.params;
+    console.log(`GET request received for /pokemon/${index} route`);
+    if (!pokemon[request.params.index]) {
+        response.send(`Sorry, no pokemon found at ${request.params.index}`)
+    } else { 
+        response.send(pokemon[request.params.index]);
+    }
 });
 
 app.get("/:verb/:adjective/:noun", (request, response) => {
     let { verb, adjective, noun } = request.params;
+    console.log(`GET request received for /${verb}-${adjective}-${noun} route`);
     response.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`);
 });
 
