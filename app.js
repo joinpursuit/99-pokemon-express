48803d00c2ab9904e79ac5d96b2c47ef3ae6e27e
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2022-01-10 05:30:30 -0500

    adds project message

--- app.js
+++ app.js
@@ -7,16 +7,21 @@ app.get("/", (request, response) => {
     response.send("Welcome 99 Pokemon");
 });
 
+app.get("/:verb/:adjective/:noun", (request, response) => {
+    let { verb, adjective, noun } = request.params;
+    response.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`);
+});
+
 app.get("/bugs", (request, response) => {
     console.log("GET request received for /bugs route");
     response.send("<h1>99 little bugs in the code<h1/>" + `<a href='http://localhost:8888/bugs/101'>Pull one down, patch it around</a>`);
   });
   
-  app.get("/bugs/:numberOfBugs", (request, response) => {
-    let { numberOfBugs } = request.params;
-    if (numberOfBugs < 200) {
-        let plusTwo = Number(numberOfBugs) + 2;
-        response.send(`${numberOfBugs} little bugs in the code <a href="http://localhost:8888/bugs/${plusTwo}">Pull one down, patch it around</a>`);
+  app.get("/bugs/:numOfBugs", (request, response) => {
+    let { numOfBugs } = request.params;
+    if (numOfBugs < 200) {
+        let plusTwo = Number(numOfBugs) + 2;
+        response.send(`${numOfBugs} little bugs in the code <a href="http://localhost:8888/bugs/${plusTwo}">Pull one down, patch it around</a>`);
     } else {
         response.send(`<a href="http://localhost:8888/bugs/">Too many bugs!! Start over!</a>`);
     }
