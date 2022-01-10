ecff36654f72f7a3306d3097533845dd6e997642
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2022-01-10 05:15:34 -0500

    adds /bug route

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const app = express();
 
 
 app.get("/", (request, response) => {
-    console.log("GET request received for ("/") route");
-    response.send("Welcome to 99 Pokemon");
+    console.log("GET request received for / route");
+    response.send("Welcome 99 Pokemon");
 });
 
+app.get("/bugs", (request, response) => {
+    console.log("GET request received for /bugs route");
+    response.send("<h1>99 little bugs in the code<h1/>" + `<a href='http://localhost:8888/bugs/101'>Pull one down, patch it around</a>`);
+  });
+  
 
 module.exports = app; 
