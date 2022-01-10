871795913b2e236ccaafd70f54411c8be2c46506
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2022-01-10 05:25:37 -0500

    completes num of bugs

--- app.js
+++ app.js
@@ -12,5 +12,14 @@ app.get("/bugs", (request, response) => {
     response.send("<h1>99 little bugs in the code<h1/>" + `<a href='http://localhost:8888/bugs/101'>Pull one down, patch it around</a>`);
   });
   
+  app.get("/bugs/:numberOfBugs", (request, response) => {
+    let { numberOfBugs } = request.params;
+    if (numberOfBugs < 200) {
+        let plusTwo = Number(numberOfBugs) + 2;
+        response.send(`${numberOfBugs} little bugs in the code <a href="http://localhost:8888/bugs/${plusTwo}">Pull one down, patch it around</a>`);
+    } else {
+        response.send(`<a href="http://localhost:8888/bugs/">Too many bugs!! Start over!</a>`);
+    }
+})
 
 module.exports = app; 
