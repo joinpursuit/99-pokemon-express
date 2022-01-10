125a9f8b22d778c447d1d4e93d617ba3d7491761
Name Ping Louie <namepinglouie@gmail.com>
2022-01-10 15:07:53 -0500

    completed: pokemon

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // DEPENDENCIES
 const express = require("express");
+const pokemon = require("./models/pokemon.json");
 
 //CONFIGURATION
 const app = express();
@@ -26,6 +27,25 @@ app.get("/bugs/:numberOfBugs", (req, res)=> {
         res.send(`<p>${numberOfBugs} little bugs in the code</p>
                   <a href="/bugs/${Number(numberOfBugs) + 2}">Pull one down, patch it around</a>`);
     }
+})
+
+console.log(pokemon[0]);
+
+app.get("/pokemon", (req, res)=> {
+    res.send(pokemon);
+})
+
+app.get("/pokemon/search", (req, res)=> {
+    const {name} = req.query;
+    const foundPoke = pokemon.filter(pokeEl => pokeEl.name.toLowerCase() === name.toLowerCase());
+    if(foundPoke.length > 0) res.send(foundPoke);
+    else res.send([]);
+})
+
+app.get("/pokemon/:indexOfArray", (req, res)=> {
+    let {indexOfArray} = req.params;
+    if(pokemon[indexOfArray]) res.send(pokemon[indexOfArray]);
+    else res.send(`Sorry, no pokemon found at ${indexOfArray}`);
 })
 
 // EXPORT
