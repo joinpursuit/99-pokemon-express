418d3bbe46c3613ef9cc1d0831e62208badf66e1
Sara Perkaj <saraperkaj@pursuit.org>
2022-01-11 02:11:35 -0500

    Not passing all tests, worked with stephanie and vanessa.

--- app.js
+++ app.js
@@ -1,1 +1,61 @@
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+const pokemon = require("./models/pokemon.json");
 
+// console.log(pokemon[0]);
+
+app.get("/", (request, respond) => {
+  respond.send("Welcome 99 Pokemon");
+});
+
+app.get("/:verb/:adjective/:noun", (request, respond) => {
+  const { verb, adjective, noun } = request.params;
+  respond.send(
+    `Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`
+  );
+});
+
+app.get("/bugs", (request, respond) => {
+  //we start on hp bugs
+  //click on link
+  //
+  respond.send(`
+  <p>99 little bugs in the code </p>
+  <a href='/bugs/101'> Pull one down, 
+  patch it around </a>
+  `);
+});
+
+app.get("/bugs/:numberOfBugs", (request, respond) => {
+  //if over 201
+  //then error link "Too many bugs!! Start over!"
+
+  const buggies = Number(request.params.numberOfBugs);
+
+  buggies > 199
+    ? respond.send(`<a href="/bugs"> Too many bugs!! Start over! </a>`)
+    : respond.send(`
+    <p>${buggies} little bugs in the code</p> 
+    <a href="/bugs/${buggies + 2}"> 
+    Pull one down, 
+    patch it around 
+    </a>
+    `);
+});
+// <p>${buggies} little bugs in the code </p>
+//   </ br>
+// <p> ${buggies} little bugs</p>
+
+app.get("/pokemon", (request, respond) => {
+  respond.send(pokemon);
+});
+
+app.get("/pokemon/:indexOfArray", (request, respond) => {
+  const { indexOfArray } = request.params;
+  pokemon[indexOfArray]
+    ? respond.send(pokemon[indexOfArray])
+    : respond.send(`Sorry, no pokemon found at ${indexOfArray}`);
+});
+
+module.exports = app;
