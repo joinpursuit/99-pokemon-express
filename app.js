bd468512f12f2b2d22f87568415264be5e94d914
TheodoreFrazier <theo.frazier@icloud.com>
2022-01-11 19:16:53 -0500

    Fixed typo to pass an additional test

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
-console.log(express)
+
 
 const pokemon = require("./models/pokemon.js")
 console.log(pokemon[1]);
 
 
 const app = express();
+
+app.get("/:verb/:noun/:adjective", (request, response) => {
+    const { verb, noun, adjective } = request.params;
+    response.send(
+        `Congratulations on starting a new project called ${verb}-${noun}-${adjective}`
+    );
+});
 
 app.get("/", (req, res) => {
     res.send("Welcome 99 Pokemon");
@@ -18,17 +25,26 @@ app.get("/bugs", (req, res) => {
         </h1>
         <a href = '/bugs/101'> Pull one down, patch it around
         </a>`
-        )
+    )
 });
 
 app.get("/pokemon", (req, res) => {
     res.send(pokemon);
 })
 
+app.get("/pokemon/:indexOfArray", (req, res) => { //request parameter 
+    const { pokeIndex } = request.params; // Object to request pokemon at index
+    if (!pokemon[pokeIndex]) {
+        this.response.send(`Sorry, no pokemon found at ${pokeIndex}`);
+    } else {
+        response.send(pokemon[pokeIndex]);
+    }
+});
+
 app.get("/pokemon/search", (req, res) => {
     res.send(
         pokemon.filter((pokeObj) => {
-            return pokeObj.name.toLowerCase() === this.request.query.name.toLowerCase()
+            return pokeObj.name.toLowerCase() === this.request.query.name.toLowerCase() //if pokeObj name is equal to the query 
         })
     );
 });
