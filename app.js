44cdd4ba27df0bc5a86b6dbf117ef8c3d9a6d608
Deborah Campos <631care@gmail.com>
2022-01-11 19:09:41 -0500

    all tests pass

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const pokemon = require("./models/pokemon.json");
 
 require("dotenv").config();
 
@@ -8,20 +9,42 @@ const PORT= process.env.PORT
 
 app.get("/", (req,res)=>{
     res.send("Welcome 99 Pokemon")
-})
+});
 
 app.get("/bugs", (req, res)=>{
     res.send("99 little bugs in the code")
+});
+
+app.get("/pokemon", (req, res)=>{
+    res.send(pokemon)
+});
+
+app.get("/pokemon/search", (req, res)=>{
+    const {name} = req.query
+    let findName= pokemon.filter((pokemon)=>{
+        return pokemon.name.toUpperCase() === name.toUpperCase()
+    })
+        res.send(findName)
 })
+
+app.get("/pokemon/:indexOfArray", (req, res)=>{
+    const {indexOfArray} = req.params 
+    if(!pokemon[indexOfArray]){
+        res.send(`Sorry, no pokemon found at ${indexOfArray}`)
+    } else {
+        res.send(pokemon[indexOfArray]);
+    }
+});
+
 app.get("/bugs/:numberOfBugs", (req,res)=>{
     const {numberOfBugs} = req.params;
-    const twoPlus = numberOfBugs + 2
+    const twoPlus = Number(numberOfBugs) + 2
     if(numberOfBugs >= 200){
         res.send("Too many bugs!! Start over!")
     } else {
-        res.send(`${numberOfBugs} little bugs in the code. pull one down, patch it around.`)
+        // res.send(`${numberOfBugs} little bugs in the code. pull one down, patch it around.`)
+        res.send(`${numberOfBugs} little bugs in the code <a href=/bugs/${twoPlus}>Pull one down, patch it around.</a>`)
     }
-    
 })
 
 app.get("/app", (req, res)=>{
