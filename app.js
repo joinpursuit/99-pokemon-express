c430c1914292ee4483b35228636829b16dfee51a
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-01-14 16:46:21 -0500

    ignore this commit

--- app.js
+++ app.js
@@ -12,31 +12,17 @@ app.get("/:verb/:adjective/:noun", (req, res) => {
     const { verb, adjective, noun } = req.params;
     if( verb && adjective && noun ) {
         res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`); 
-    } else if ( verb && adjective ){
-        res.send(`Congratulations on starting a new project called ${verb}-${adjective}!`); 
-    } else if ( verb && noun ){
-        res.send(`Congratulations on starting a new project called ${verb}-${noun}!`); 
-    } else if ( noun && adjective ){
-        res.send(`Congratulations on starting a new project called ${adjective}-${noun}!`); 
     } else {
         res.send(`No new project defined in the form of verb-adjective-noun`);
     }
 });
-
-//~99 little bugs 
-// 99 Little Bugs In the Code
-// 99 little bugs in the code
-// 99 little bugs
-// Pull one down
-// Patch it around
-// 101 bugs in the code
 
 // On the home page (get "/bugs"), users should see:
 // "99 little bugs in the code"
 // a link that says "pull one down, patch it around"
 // this should link to /bugs/101, where the number represents the number of bugs remaining to fix
 // When a number is given in the url (get "/bugs/:numberOfBugs"), users should see:
-// The number of bugs left in the code (i.e. 101 little bugs in the code)
+// The number of bugs left in the code (i.e. 101 little bugs in the code);
 
 
 // a link to "pull one down, patch it around", where the href is number of bottles in the parameter plus 2
@@ -58,8 +44,7 @@ app.get("/bugs/:numberOfBugs", (req, res) => {
     } else {
 
     }
-
 })
 
-//export 
+
 module.exports = app;
