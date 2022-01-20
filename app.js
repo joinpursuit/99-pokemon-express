e279cbec210ebf5ce7e89a110ec2d41377916c07
shareekaepps <shareekaepps@icloud.com>
2022-01-20 00:54:42 -0500

    fixed bugs, tests passed

--- app.js
+++ app.js
@@ -8,53 +8,57 @@ require("dotenv").config();
 const app = express();
 
 // ROUTES
-app.get("/", (require, response) => {
-    response.send("Welcome 99 Pokemon");
+app.get("/", (req, res) => {
+    console.log("GET to /")
+    res.send("Welcome 99 Pokemon");
 })
 
 //EXPRESS WILDCARD VARIABLES
-app.get("/:verb/:adjective/:noun", (require, response) => {
-    response.send(`Congratulations on starting a new project called ${require.params.verb}-${require.params.adjective}-${require.params.noun}!`);
+app.get("/:verb/:adjective/:noun", (req, res) => {
+    // const { verb, adjective, noun } = req.params;
+    res.send(`Congratulations on starting a new project called ${req.params.verb}-${req.params.adjective}-${req.params.noun}!`);
 })
 
 
 // 99 BUGGIES CODE
-app.get("/bugs", (require, response) => {
-    console.log("GET request recieved to route: /bugs");
-    
-    response.send(
-    `<h1>"99 little bugs in the code"</h1>
-    <a href= "/bugs/101"> Pull one down, patch it around </a>`)
+
+app.get("/bugs", (req, res) => {
+    res.send(
+    `<h1>99 little bugs in the code</h1>
+    <a href= "/bugs/101"> Pull one down, patch it around</a>`
+    );
 })
 
-app.get("/bugs/:numberOfBugs", (request, response) => {
-    const { numberOfBugs } = request.params;
+app.get("/bugs/:numberOfBugs", (req, res) => {
+    const { numberOfBugs } = req.params;
     const coolBug = Number(numberOfBugs);
     
-    response.send(
-    `<p> ${coolBug} little bugs in the code </p>`
-    `<a href= "${coolBug < 200 ? coolBug + 2 : "./"}" >
-    ${coolBug < 200 ? "Pull one down, patch it around"
-    : "Too many bugs!! Start over!"}
-    </a>` );
-})
+    res.send(
+    `<p> ${coolBug} little bugs in the code </p>
+     <a href= "${coolBug < 200 ? coolBug + 2 : "./"}" >${coolBug < 200 
+     ? "Pull one down, patch it around"
+     : "Too many bugs!! Start over!"}</a>`
+    );
+});
+
+
 
 // POKEMON, GOTTA CATCH EM' ALL
-app.get("/pokemon/search", (request, response) => {
-    const { name } = response.query;
-    const selectPoke = pokemon.find((pokemon) => pokemon.name.toLowerCase()
-    === name.toLowerCase());
-
-    response.send(selectPoke ? [selectPoke] : []);
+app.get("/pokemon", (req, res) => {
+    res.send(pokemon);
 })
 
+app.get("/pokemon/search", (req, res) => {
+    const { name } = req.query;
+    const selectPoke = pokemon.find((pokemon) => pokemon.name.toLowerCase() === name.toLowerCase());
 
-
-app.get("/pokemon/:index", (request, response) => {
-    const { index } = request.params;
-    response.send(pokemon[Number(index)] || "No pokemon found at " + index );
+    res.send(selectPoke ? [selectPoke] : []);
 })
 
+app.get("/pokemon/:index", (req, res) => {
+    const { index } = req.params;
+    res.send(pokemon[Number(index)] || "Sorry, no pokemon found at " + index );
+})
 
 
 module.exports = app;
