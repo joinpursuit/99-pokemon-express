5314e8a2590ac896a5d129b3fbf7712d3560205d
Lortiz528 <lortiztech@gmail.com>
2022-07-13 12:32:52 -0400

    99 bugs solved

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Welcome 99 Pokemon');
+});
+
 app.get('/:verb/:adjective/:noun', (req, res) => {
   const { verb, adjective, noun } = req.params;
   if (verb === 'jumping' && adjective === 'joyous' && noun === 'jellybean') {
@@ -12,6 +16,26 @@ app.get('/:verb/:adjective/:noun', (req, res) => {
     res.send(
       `Congratulations on starting a new project called ${verb} ${adjective} ${noun}!`
     );
+  }
+});
+
+app.get('/bugs', (req, res) => {
+  res.send(`<h1>99 little bugs in the code</h1>
+    <a href="/bugs/101">pull one down, patch it around </a>`);
+});
+
+app.get('/bugs/:numberOfBugs', (req, res) => {
+  let { numberOfBugs } = req.params;
+
+  let numbugs = Number(numberOfBugs);
+
+  if (numbugs < 200) {
+    res.send(`<h1>${numbugs} little bugs in the code</h1>
+      <a href="/bugs/${numbugs + 2}">Pull one down, patch it around</a> `);
+  } else {
+    res.send(`
+        <h1>${numbugs} little bugs in the code</h1>
+        <a href="/bugs">Too many bugs!! Start over!</a>`);
   }
 });
 
