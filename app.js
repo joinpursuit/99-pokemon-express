4e880e1b7f7125941b1934c5aec68ad5ffa1a9e4
JuHaoChen1997 <juhaochen97@gmail.com>
2022-07-13 16:28:16 -0400

    pass bugs test

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const express = require("express");
+const { download } = require("express/lib/response");
 const app = express();
 
 require("dotenv").config();
 const PORT = process.env.PORT;
+
+app.get("/bugs/:numberOfBugs", (req, res) => {
+  const { numberOfBugs } = req.params;
+
+  if (parseInt(numberOfBugs) >= 200) {
+    res.send(`<a href="/">Too many bugs!! Start over!</a>`);
+  } else {
+    res.send(
+      `<a href="/bugs/${
+        parseInt(numberOfBugs) + 2
+      }">${numberOfBugs} little bugs in the code Pull one down, patch it around</a>`
+    );
+  }
+});
+
+app.get("/bugs", (req, res) => {
+  res.send(
+    `<h1>99 little bugs in the code</h1><a href="/bugs/101">pull one down, patch it around</a>`
+  );
+});
 
 app.get("/:verb/:adjective/:noun", (req, res) => {
   const { verb, adjective, noun } = req.params;
