0e1821700926222985f6bb0d4ee7b22dd02e122a
Jonathan Scheiber <jon@scheiber.me>
2022-07-13 12:57:52 -0400

    complete 99 little bugs in the code

--- app.js
+++ app.js
@@ -10,4 +10,27 @@ app.get("/:verb/:adjective/:noun", (req, res) => {
   );
 });
 
+// 99 Little Bugs In the Code
+app.get("/bugs", (req, res) => {
+  res.send(`<h1>99 little bugs in the code</br>
+99 little bugs</br>
+<a href="./101">Pull one down, patch it around</a></br>
+101 bugs in the code</h1>`);
+});
+
+app.get("/bugs/:numberOfBugs", (req, res) => {
+  const { numberOfBugs } = req.params;
+  const numberOfBugsPlusTwo = Number(numberOfBugs) + 2;
+  if (numberOfBugs >= 200) {
+    res.send('<h1><a href="./">Too many bugs!! Start over!</a></br></h1>');
+  } else {
+    res.send(
+      `<h1><p>${numberOfBugs} little bugs in the code</br>
+${numberOfBugs} little bugs</br>
+<a href="./${numberOfBugsPlusTwo}">Pull one down, patch it around</a></br>
+${numberOfBugsPlusTwo} bugs in the code</p></h1>`
+    );
+  }
+});
+
 module.exports = app;
