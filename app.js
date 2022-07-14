ca76fd132d71ba6f6d26ce9b97cfaa608f666966
T.K <nique@niques-air.myfiosgateway.com>
2022-07-13 22:18:14 -0400

    adding link with number of bugs plus 2

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Welcome 99 Pokemon")
+});
 
+app.get("/bugs", (req, res) => {
+    res.send("<h1>99 little bugs in the code</h1><a href='/bugs/101'>pull one down, patch it around</a>")
+});
 
+app.get("/bugs/:numberOfBugs", (req, res) => {
+    // console.log(req.params)
+    const { numberOfBugs } = req.params;
+    // const newNum = num + 2;
+    const bugNum = Number(numberOfBugs)
+    if ( bugNum < 200) {
+        res.send(`${bugNum} little bugs in the code`)
+    } else {
+        res.send("<a href='/'>Too many bugs!! Start over!</a>")
+    }
+});
 
 
 
