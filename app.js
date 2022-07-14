2c6aa428a48ec54cafa4f9b3767f32be841228a4
named.josie <named.josie@gmail.com>
2022-07-13 20:23:22 -0400

    updated files

--- app.js
+++ app.js
@@ -2,34 +2,6 @@ const express = require('express')
 
 const app = express()
 
-app.get('/:verb/:adjective/:noun', (req, res) => {
-  const { verb, adjective, noun } = req.params
-
-  res.send(
-    `Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`
-  )
-})
-
-app.get('/bugs', (req, res) => {
-  res.send(
-    '<h1>99 little bugs in the code</h1> <a href=/bugs/101>Pull one down, patch it around</a>'
-  )
-})
-
-app.get('/bugs/:numberOfBugs', (req, res) => {
-  const { numberOfBugs } = req.params
-
-  if (numberOfBugs >= 200) {
-    res.send(`<a href=/bugs>Too many bugs!! Start over!</a>`)
-  } else {
-    res.send(
-      `<p>${numberOfBugs} little bugs in the code</p> <a href=/bugs/${
-        Number(numberOfBugs) + 2
-      }>Pull one down, patch it around</a>`
-    )
-  }
-})
-
 const pokemon = require('./models/pokemon.json')
 
 app.get('/', (req, res) => {
@@ -63,5 +35,38 @@ app.get('/pokemon/:indexOfArray', (req, res) => {
     res.send(`Sorry, no pokemon found at ${Number(indexOfArray)}`)
   }
 })
+
+
+
+
+
+app.get('/:verb/:adjective/:noun', (req, res) => {
+  const { verb, adjective, noun } = req.params
+
+  res.send(
+    `Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`
+  )
+})
+
+app.get('/bugs', (req, res) => {
+  res.send(
+    '<h1>99 little bugs in the code</h1> <a href=/bugs/101>Pull one down, patch it around</a>'
+  )
+})
+
+app.get('/bugs/:numberOfBugs', (req, res) => {
+  const { numberOfBugs } = req.params
+
+  if (numberOfBugs >= 200) {
+    res.send(`<a href=/bugs>Too many bugs!! Start over!</a>`)
+  } else {
+    res.send(
+      `<p>${numberOfBugs} little bugs in the code</p> <a href=/bugs/${
+        Number(numberOfBugs) + 2
+      }>Pull one down, patch it around</a>`
+    )
+  }
+})
+
 
 module.exports = app
