01d79b71be4528a14514475d93f3fc95130acb41
Andres Varon <anvaron@gmail.com>
2022-07-14 13:02:42 -0500

    Poke-Express - fixed bug

--- app.js
+++ app.js
@@ -1,67 +1,75 @@
- //
-const express = require('express');
-const pokemon = require("./models/pokemon.json");
-console.log(pokemon[0]);
-
+const express = require("express");
 const app = express();
+
+require("dotenv").config();
+//const PORT = process.env.PORT;
+
+const pokemon = require("./models/pokemon.json");
 
 // Routes
 
 // Home
-app.get('/', (req, res) => {
-  res.send('Welcome 99 Pokemon App')
-})
+app.get("/", (req, res) => {
+	res.send("Welcome 99 Pokemon");
+});
 
-// New Project Name Generator route
-app.get('/:verb/:adjective/:noun', (req, res) => {
-  const { verb, adjective, noun } = request.params;
+// New Project Name Generator
+app.get("/:verb/:adjective/:noun", (req, res) => {
+	const { verb, adjective, noun } = req.params;
+	res.send(
+    `Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`
+    );
+});
 
-  res.send(`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`)
-})
+//99 Little bugs in the code
+app.get("/bugs", (req, res) => {
+	res.send(
+    `99 little bugs in the code 
+    \n 99 little bugs 
+    \n Pull one down, patch it around
+    <a href="/bugs/101">Pull one down, patch it around</a>`
+  );
+});
+app.get("/bugs/:numberOfBugs", (req, res) => {
+	const { numberOfBugs } = req.params;
+	// Validating num of bugs
+	(Number(numberOfBugs) < 200) 
+  ?
+		res.send(
+      `<h1>${numberOfBugs} little bugs in the code ${numberOfBugs} little bugs</h1> 
+      <a href="/bugs/${Number(numberOfBugs) + 2}">Pull one down, patch it around</a>`
+    )
+  :    
+		res.send(`Too many bugs!! Start over!
+      <a href="/bugs">Start over</a>`
+    )
+});
 
-// 99 Little Bugs In the Code routes
-//
-app.get('/bugs', (req, res) => {
-  res.send(`<p>99 little bugs in the code</p><a href="/bugs/${getNumber(0, 101)}">pull one down, patch it around</a>`)
-})
-// 
-app.get('/bugs/:numberOfBugs', (req, res) => {
-  const { numberOfBugs } = req.params;
+//Pokemon
+app.get("/pokemon", (req, res) => {
+	res.send(pokemon);
+});
 
-  res.send(`<p>${numberOfBugs} little bugs in the code</p>${validateNumOfBugs(numberOfBugs)}`)
-})
+app.get("/pokemon/search", (request, response) => {
+	const { name } = request.query;
+	let foundPokemon = pokemon.find((poke) => {
+		return poke.name.toLowerCase() === name.toLowerCase();
+	});
+	if (foundPokemon != undefined) {
+		let dummy = [foundPokemon];
+		response.send(dummy);
+	} else {
+		response.send([]);
+	}
+});
 
-
-
-/**
- ******************************************************************
- * * * * H E L P E R S * * *
- ******************************************************************
- */
-
- /**
- * [getNumber() function generates a random index between 0 and 101]
- * @param  {number} min [min value equals to be generated]
- * @param  {number} max [max value equals to be generated]
- * @return {number}     [Number that represents a number of bugs]
- */
- function getNumber(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); 
-}
-
-/**
- * [validateNumOfBugs() function generates an anchor according the propper validation]
- * @param  {number} min [number that represents the current number of bugs]
- * @return {string}     [Dynamic anchor after validating params]
- */
-function validateNumOfBugs(bugs) {
-  bugs = (Number(bugs) + 2);
-  return (bugs < 200) ?
-                      `<a href="/bugs/${bugs}">pull one down, patch it around</a>`
-                      :
-                      `<a href="/bugs">Start over</a>`
-}
+app.get("/pokemon/:indexOfArray", (request, response) => {
+	const { indexOfArray } = request.params;
+	if (pokemon[indexOfArray]) {
+		response.send(pokemon[indexOfArray]);
+	} else {
+		response.send(`Sorry, no pokemon found at ${indexOfArray}`);
+	}
+});
 
 module.exports = app;
