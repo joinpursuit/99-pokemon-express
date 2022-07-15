318882ee4c9999366548a5154456beb002c87c63
T.K <nique@niques-air.myfiosgateway.com>
2022-07-14 21:40:27 -0400

    completed lab

--- app.js
+++ app.js
@@ -1,28 +1,61 @@
 const express = require("express");
 const app = express();
+const pokemon = require("./models/pokemon.json");
+// console.log(pokemon[0]);
 
 app.get("/", (req, res) => {
-    res.send("Welcome 99 Pokemon")
+  res.send("Welcome 99 Pokemon");
 });
 
 app.get("/bugs", (req, res) => {
-    res.send("<h1>99 little bugs in the code</h1><a href='/bugs/101'>pull one down, patch it around</a>")
+  res.send(
+    "<h1>99 little bugs in the code</h1><a href='/bugs/101'>pull one down, patch it around</a>"
+  );
 });
 
 app.get("/bugs/:numberOfBugs", (req, res) => {
-    // console.log(req.params)
-    const { numberOfBugs } = req.params;
-    const bugNum = Number(numberOfBugs)
-    const newNum = bugNum + 2;
-    if ( bugNum < 200) {
-        res.send(`${bugNum} little bugs in the code`)
-    } else {
-        res.send("<a href='/'>Too many bugs!! Start over!</a>")
-    }
+  // console.log(req.params)
+  const { numberOfBugs } = req.params;
+  if (numberOfBugs < 200) {
+    res.send(
+      `${numberOfBugs} little bugs in the code <a href=${
+        Number(numberOfBugs) + 2
+      }>Pull one down, patch it around</a>`
+    );
+  } else {
+    res.send("<a href='/'>Too many bugs!! Start over!</a>");
+  }
 });
 
+app.get("/pokemon", (req, res) => {
+  res.send(pokemon);
+});
 
+app.get("/pokemon/search", (req, res) => {
+  const { name } = req.query;
+  if (!pokemon.filter((poke) => poke.name === name)) {
+    res.send("[]");
+  } else {
+    res.send(
+      pokemon.filter((poke) => poke.name.toLowerCase() === name.toLowerCase())
+    );
+  }
+});
 
+app.get("/pokemon/:indexOfArray", (req, res) => {
+  const { indexOfArray } = req.params;
+  if (pokemon[indexOfArray]) {
+    res.send(pokemon[indexOfArray]);
+  } else {
+    res.send(`Sorry, no pokemon found at ${indexOfArray}`);
+  }
+});
 
+app.get("/:verb/:adjective/:noun", (req, res) => {
+  const { verb, adjective, noun } = req.params;
+  res.send(
+    `Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`
+  );
+});
 
 module.exports = app;
